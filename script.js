7372227944b37c2d0a3f52cc0774bcdff2dd0b65
cmmisura14 <cmmisura14@gmail.com>
2015-05-04 11:58:38 -0500

    add background texture and search functionality

--- script.js
+++ script.js
@@ -2,17 +2,50 @@
  * Created by codymisura on 5/1/15.
  */
 $(document).ready(function(){
-    $.ajax({
-        type: 'GET',
-        url: 'https://api.github.com/users/cmmisura14/orgs/repos',
-        crossDomain: true,
-        Accept: "application/vnd.github.v3+json",
-        dataType: 'text',
-        success: function (response) {
-            console.log(response);
-        },
-        error: function (xhr, status) {
-            alert('Error: ' + status);
-        }
+
+    var username;
+    var image;
+    var projects;
+    var githubPage;
+
+    function searchCallback(results){
+
+        username = results.name;
+        image = results.avatar_url;
+        projects = results.repos_url;
+        githubPage = "https://github.com/users/" + $("#searchField").val();
+
+        $("#result").append("<p class='lead'><a href='" + githubPage + "'>Name: " + username + "</a></p><img class ='profilePic' src='" + image + "'>");
+
+
+    }
+   function search(query){
+       $.ajax({
+           type: 'GET',
+           url: 'https://api.github.com/users/' + encodeURI(query) + '?client_id=f8a4b95805c9804c9eb7&client_secret=4b1bff35a5b8b802fe4bb4e1204afd2f56fc8d8d',
+           crossDomain: true,
+           Accept: "application/vnd.github.v3+json",
+           dataType: 'JSON',
+           complete : function (response) {
+               console.log('ajax complete');
+           },
+           success : function (data) {
+               console.log(data);
+               searchCallback(data);
+           },
+           error: function (xhr, status) {
+               alert('Error: ' + status);
+           }
+       });
+   }
+
+
+
+    $(".btn").on("click", function(){
+        $("#result").empty();
+        var userSearch = $("#searchField").val();
+        $("#searchField").val('');
+
+        search(userSearch);
     });
 });
